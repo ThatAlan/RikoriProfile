5fb6d8bd981d891efbc864eda57ab51e11bedb21
Mallow <72262190+mallowv@users.noreply.github.com>
2022-12-27 00:45:42 +0700

    Removed extraneous Code

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 import './style.scss'
-import './bg/stars.scss'
-import { generateStars } from './bg/stars'
 
 
 new Typewriter("#typewriter", {
@@ -8,28 +6,3 @@ new Typewriter("#typewriter", {
   autoStart: true,
   loop: true
 });
-
-var starsPosition = generateStars();
-
-const maxStarsPosition = 700;
-const minStarsPosition = 0;
-
-export const generateCometaFromStar = function(starsLocation) {
-  let position = Math.floor(
-    Math.random() * (maxStarsPosition - minStarsPosition) + minStarsPosition
-  );
-  let coord = starsLocation[position];
-  let element = document.getElementsByClassName("star-type1 " + position)[0];
-  if (element.classList.contains("animated")) {
-    element.classList.remove("animated");
-  }
-
-  //add start light
-  setTimeout(() => {
-    element.classList.add("animated-cometa");
-  }, position * 1000);
-};
-
-for (let count = 0; count < 4; count++) {
-  generateCometaFromStar(starsPosition);
-}
